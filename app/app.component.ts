426e47017e8cdb697cee69df9c7fb690e9d24abf
StephanK9 <stephane.khoury@gmail.com>
2017-01-20 11:00:06 -0800

    add property binding to our meal tracker app to display the range of calories for a food item

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
     <h1>Meal Tracker App for {{month}}/{{day}}/{{year}}</h1>
     <h3>{{currentFocus}}</h3>
     <ul>
-      <li *ngFor="let currentFood of foods">{{currentFood.description}}</li>
+      <li [class]="caloriesColor(currentFood)" (click)="isDone(currentFood)" *ngFor="let currentFood of foods">{{currentFood.description}}<br>Calories:{{currentFood.calories}} <button (click)="editFood()">Edit!</button></li>
     </ul>
   </div>
   `
@@ -20,13 +20,36 @@ export class AppComponent {
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
   foods: Food[] = [
-    new Food('Pasta with butter and cheese'),
-    new Food('Chicken with rice'),
-    new Food('Soup for da soul')
+    new Food('Pasta with butter and cheese', 600),
+    new Food('Chicken with rice', 430),
+    new Food('Soup for da soul', 200)
   ];
+
+  editFood() {
+    alert("You just requested to edit your Food!");
+  }
+
+  isDone(clickedFood: Food) {
+    if(clickedFood.done === true) {
+      alert("This food is done!");
+    } else {
+      alert("This food isn't done being edited yet!");
+    }
+  }
+
+  caloriesColor(currentFood){
+    if(currentFood.calories >= 500){
+      return "bg-danger";
+    } else if (currentFood.calories < 500){
+      return "bg-warning";
+    } else {
+      return "bg-info";
+    }
+  }
+
 }
 
 export class Food {
   public done: boolean = false;
-  constructor(public description: string) { }
+  constructor(public description: string, public calories: number) { }
 }
