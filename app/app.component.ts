21ac940ca6fe80e30baf613fe470d757b35a13d2
StephanK9 <stephane.khoury@gmail.com>
2017-01-20 10:39:24 -0800

    add display information for the templates

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,11 +4,23 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
   <div class="container">
-    <h1>Meal Tracker App</h1>
+    <h1>Meal Tracker App for {{month}}/{{day}}/{{year}}</h1>
+    <h3>{{currentFocus}}</h3>
+    <ul>
+      <li>{{firstFood.description}}</li>
+    </ul>
   </div>
   `
 })
 
 export class AppComponent {
+  currentFocus: string = 'Food Logged';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+  firstFood = {
+    description: "Pasta"
+  }
 
 }
